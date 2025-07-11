30cdb2be048b06f1cbd7374c8c9c302d2c3d85f3
kohahost <kohahost@gmail.com>
2025-07-11 12:13:41 +0700

    Delete src/components/ui directory

--- src/components/ui/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export const Input = React.forwardRef(({ className, type, ...props }, ref) = (
-  input
-    type={type}
-    className={`flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background fileborder-0 filebg-transparent filetext-sm filefont-medium placeholdertext-muted-foreground focus-visibleoutline-none focus-visiblering-2 focus-visiblering-ring focus-visiblering-offset-2 disabledcursor-not-allowed disabledopacity-50 ${className}`}
-    ref={ref}
-    {...props}
-  
-));
